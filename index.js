454d37fdfd8f5c4fcf6d44a7ec3d055ba9dad70d
Alex Siu <atenthirtyone@gmail.com>
2018-08-17 00:06:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name)
+{
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name)
+{
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver()
+{
+drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver()
+{
+  drivers.shift();
+}
+
+function AppendDriver(name)
+{
+ const driversB = drivers.slice();
+ driversB.push(name);
+ return driversB;
+}
